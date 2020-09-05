5b70df82e23c52d925a30e17146bce66b0de7684
victorchen01 <victor.chen1447@gmail.com>
2020-09-05 13:46:22 -0400

    stufgf

--- tutor-share/src/App.js
+++ tutor-share/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test'
 
-function App() {
+const INITIAL_STATE = {
+  message: "hi"
+}
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = INITIAL_STATE;
+  }
+
+  editMessage(newMessage) {
+    this.setState({message: newMessage})
+  }
+
+  render() {
   return (
     <div className="App">
+    <Test message="lmao"/>
+    <Test message="klkl"/>
+    <Test message="kk"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1
+        onClick={() => this.editMessage('lol')}
         >
           Learn React
-        </a>
+        </h1>
       </header>
     </div>
   );
+}
 }
 
 export default App;
