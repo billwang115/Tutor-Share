ced273bfb25b58065589d7154a030f1123054b6b
victorchen01 <victor.chen1447@gmail.com>
2020-09-05 23:09:56 -0400

    profile

--- tutor-share/src/components/Profile.js
+++ tutor-share/src/components/Profile.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import ProfileCard from './ProfileCard';
+import AppleIcon from '@material-ui/icons/Apple';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import WebIcon from '@material-ui/icons/Web';
+import CodeIcon from '@material-ui/icons/Code';
 
 const INITIAL_STATE = {
 }
@@ -12,10 +17,19 @@ class Profile extends React.Component {
 
   render() {
   return (
-    <div>
-      
-      profile
+    <div style={{marginBottom: 80, marginTop: 50}}>
 
+      <ProfileCard
+        title="Victor"
+        desc="Slat"
+        icon=<AppleIcon />
+        label="iOS development"
+        icon2=<YouTubeIcon />
+        label2="Video Editing"
+        icon3=<CodeIcon />
+        label3="Web Development"
+        src="https://scontent.fybz2-2.fna.fbcdn.net/v/t1.15752-9/118812307_369646424068062_3732024978468783710_n.jpg?_nc_cat=102&_nc_sid=b96e70&_nc_ohc=xLXLh2-mQh8AX_0BmkT&_nc_ht=scontent.fybz2-2.fna&oh=18b7b94f68c5e244c2a1aed2dc5218ed&oe=5F77C78D"
+      />
 
     </div>
   );
